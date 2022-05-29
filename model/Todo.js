d316adbca046d1c1490bc89bfb76cd5bc30ceda6
agmorrow <morrow.austin5@gmail.com>
2022-05-29 12:21:41 -0600

    added seeds file

--- model/Todo.js
+++ model/Todo.js
@@ -2,6 +2,12 @@ const { Schema, model } = require('mongoose');
 
 //Todo Schema
 const todoSchema = new Schema({
+  userId: {
+    // ObjectId type
+    type: Schema.Types.ObjectId,
+    // Ref means which Collection does this ObjectId reference?
+    ref: 'User',
+  },
   text: {
     type: String,
     require: true,
---
+++ seeds.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const {
+  User,
+  Todo
+} = require('./model');
+
+
+const seedDb = async () => {
+  await mongoose.connect('mongodb://localhost:27017/mongoAppTodo');
+  await User.deleteMany({});
+  await Todo.deleteMany({});
+
+  process.exit(0);
+};
+
+seedDb();
+
