0a6567978ce0eba5d601b34a9edffa0eb19ac830
agmorrow <morrow.austin5@gmail.com>
2022-05-28 18:20:17 -0600

    CRUD todos

---
+++ controllers/todoController.js
@@ -0,0 +1,69 @@
+const {
+  Todo
+} = require('../model');
+
+module.exports = {
+  createTodo: async (req, res) => {
+    const {
+      text,
+      completed
+    } = req.body;
+
+    try {
+      const newTodo = await Todo.create({
+        text,
+        completed
+      });
+      res.json(newTodo);
+    } catch (e) {
+      res.json(err);
+    }
+  },
+  getAllTodos: async (req, res) => {
+    try {
+      const todos = await Todo.find();
+      res.json(todos);
+    } catch (e) {
+      res.json(e);
+    }
+  },
+  getTodoById: async (req, res) => {
+    const {
+      todoId
+    } = req.params;
+    try {
+      const todo = await Todo.findById(todoId);
+      res.json(todo);
+    } catch (e) {
+      res.json(e);
+    }
+  },
+  updateTodoById: async (req, res) => {
+    const {
+      todoId
+    } = req.params;
+    try {
+      const updatedTodo = await Todo.findByIdAndUpdate(
+        todoId, {
+          ...req.body
+        }, {
+          new: true,
+        }
+      );
+      res.json(updatedTodo);
+    } catch (e) {
+      res.json(e);
+    }
+  },
+  deleteTodoById: async (req, res) => {
+    const {
+      todoId
+    } = req.params;
+    try {
+      const deleteTodo = await Todo.findByIdAndDelete(todoId);
+      res.json(deleteTodo);
+    } catch (e) {
+      res.json(e);
+    }
+  }
+}
---
+++ routes/api/todo/index.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const {createTodo,
+  getAllTodos,
+  getTodoById,
+  updateTodoById,
+  deleteTodoById,
+} = require('../../../controllers/todoController');
+
+router.route ('/')
+  .get(getAllTodos)
+  .post(createTodo);
+
+  router.route('/:todoId')
+    .get(getTodoById)
+    .put(updateTodoById)
+    .delete(deleteTodoById);
+
+    module.exports = router;
